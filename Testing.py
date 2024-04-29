b3bea4532c97edf55be77e4316ce8d918a2d805f
Brian Slickis <bslickis@gmail.com>
2024-04-29 14:23:19 -0400

    Messing around...

---
+++ Testing.py
@@ -0,0 +1,10 @@
+from g4f.client import Client
+from g4f.Provider import RetryProvider
+
+
+client = Client()
+response = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[{"role": "user", "content": "Hello"}],
+)
+print(response.choices[0].message.content)
